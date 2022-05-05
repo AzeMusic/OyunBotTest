c87c50f04c56e5212b1df20934d575e434a453bd
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-05-05 17:13:27 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "5069143652:AAGX20dtalGq0By1kPdOfr0JqswvyIaDblU"
+TOKEN = "5181146001:AAGKo1_kDJlZeAqrcSi4HSpqAq7Qm4AAkzg"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
